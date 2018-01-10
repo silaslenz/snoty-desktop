665b66269ced42a7fce0987bd8fea0621e0af571
Silas Lenz <silas.lenz@gmail.com>
2018-01-10 16:56:26 +0100

    Ignore incorrect certificates in demo client, due to requiering file instead of keyring

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Require a certificate from the server. We used a self-signed certificate
 # so here ca_certs must be the server certificate itself.
 ssl_sock = ssl.wrap_socket(s,
-                           ca_certs="cert.pem",
-                           cert_reqs=ssl.CERT_REQUIRED)
+                           ca_certs="cert.pem")
+#                           cert_reqs=ssl.CERT_REQUIRED) # Ignore for now, requires file
 
 ssl_sock.connect(('localhost', 8000))
 
